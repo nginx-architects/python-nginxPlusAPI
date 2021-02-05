5dd2bee445c6471b43140c98367d0115fe4223a2
James Jones <jam.jones@f5.com>
2021-02-05 04:08:31 -0500

    Getting ready for packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nginxPlusAPI", # Replace with your own username
+    version="0.0.1",
+    author="James Jones",
+    author_email="jam.jones@f5.com",
+    description="Python API Client for NGINX Plus API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/outcast/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Freely Distributable",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
