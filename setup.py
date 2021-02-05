288968d85370afea24fe9b83be42386cb57c3093
James Jones <jam.jones@f5.com>
2021-02-05 04:26:37 -0500

    setting github url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Python API Client for NGINX Plus API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/outcast/",
+    url="https://github.com/nginx-architects/python-nginxPlusAPI",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
